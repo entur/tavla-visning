0b3d9acf9b48c3680ef9f1bcd9f9cbe5ee0f99e6
JakobNH <jakobnhelgoy@gmail.com>
2025-11-21 15:20:57 +0100

    Try different syntax in ts conf and prettier tile tsx (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import legacy from "@vitejs/plugin-legacy";
-import * as path from 'path'
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ["chrome 49"],
+      targets: ["chrome>=49"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
       renderLegacyChunks: true,
       modernPolyfills: true,
@@ -23,17 +23,17 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@src': path.resolve(__dirname, './src'),
-      '@shared': path.resolve(__dirname, './src/Shared'),
-      '@board': path.resolve(__dirname, './src/Board'),
-      '@components': path.resolve(__dirname, './src/Shared/components'),
-      '@hooks': path.resolve(__dirname, './src/Shared/hooks'),
-      '@utils': path.resolve(__dirname, './src/Shared/utils'),
-      '@types': path.resolve(__dirname, './src/Shared/types'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@graphql': path.resolve(__dirname, './src/Shared/graphql'),
-      '@assets': path.resolve(__dirname, './src/Shared/assets'),
+      "@": path.resolve(__dirname, "./src"),
+      "@src": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "./src/Shared"),
+      "@board": path.resolve(__dirname, "./src/Board"),
+      "@components": path.resolve(__dirname, "./src/Shared/components"),
+      "@hooks": path.resolve(__dirname, "./src/Shared/hooks"),
+      "@utils": path.resolve(__dirname, "./src/Shared/utils"),
+      "@types": path.resolve(__dirname, "./src/Shared/types"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@graphql": path.resolve(__dirname, "./src/Shared/graphql"),
+      "@assets": path.resolve(__dirname, "./src/Shared/assets"),
     },
   },
 });
