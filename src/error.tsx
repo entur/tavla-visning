3d6aa76f06f71d075c2f71d18fa8c6eaf26076e7
Silje <31701059+siljemoksnes@users.noreply.github.com>
2026-01-06 10:42:57 +0100

    :art: legg til finere error-skjerm

--- src/error.tsx
+++ src/error.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@entur/button'
+import { Heading3 } from '@entur/typography'
+import BeaverIllustration from './Shared/assets/illustrations/BeaverIllustration.png'
 import { Loader } from '@entur/loader'
 import type { BoardDB } from './Shared/types/db-types/boards'
 
@@ -5,9 +8,10 @@ interface BoardStatusProps {
 	loading: boolean
 	error: string | null
 	board: BoardDB | null
+	theme?: string
 }
 
-export function BoardStatus({ loading, error, board }: BoardStatusProps) {
+export function BoardStatus({ loading, error, board, theme }: BoardStatusProps) {
 	const containerClass = 'flex h-screen w-full items-center flex-col justify-center text-2xl'
 
 	if (loading) {
@@ -20,7 +24,23 @@ export function BoardStatus({ loading, error, board }: BoardStatusProps) {
 	}
 
 	if (error) {
-		return <div className={containerClass}>Feil ved lasting av tavle: {error}</div>
+		return (
+			<div className="mx-auto flex h-[70vh] flex-col items-center justify-center gap-5 lg:w-1/4">
+				<Heading3 data-color-mode={theme} className="bg-transparent">
+					Ups, denne tavla finnes ikke!
+				</Heading3>
+
+				<img
+					src={BeaverIllustration}
+					className="w-1/2 lg:w-1/2"
+					alt="Illustrasjon av en forundret bever"
+				/>
+
+				<Button href="https://tavla.entur.no" variant="primary" as="a" data-color-mode={theme}>
+					Tilbake til forsiden
+				</Button>
+			</div>
+		)
 	}
 
 	if (!board) {
