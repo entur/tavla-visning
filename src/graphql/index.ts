46c321adb8da446f55451f68dc34ef40e281348a
Guro Brøto <guro.broto@bekk.no>
2026-02-05 10:01:35 +0100

    Formatter index graphql fil

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,50 +1,260 @@
 // @ts-nocheck
-import type * as Types from 'src/types/graphql-schema';
+import type * as Types from 'src/types/graphql-schema'
 
-import type { DocumentTypeDecoration } from '@graphql-typed-document-node/core';
-export type TDepartureFragment = { __typename?: 'EstimatedCall', aimedDepartureTime: DateTime, expectedDepartureTime: DateTime, expectedArrivalTime: DateTime, cancellation: boolean, realtime: boolean, quay: { __typename?: 'Quay', publicCode: string | null, name: string }, destinationDisplay: { __typename?: 'DestinationDisplay', frontText: string | null, via: Array<string | null> | null } | null, serviceJourney: { __typename?: 'ServiceJourney', id: string, transportMode: Types.TTransportMode | null, transportSubmode: Types.TTransportSubmode | null, line: { __typename?: 'Line', id: string, publicCode: string | null, presentation: { __typename?: 'Presentation', textColour: string | null, colour: string | null } | null } }, situations: Array<{ __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> }> };
+import type { DocumentTypeDecoration } from '@graphql-typed-document-node/core'
+export type TDepartureFragment = {
+	__typename?: 'EstimatedCall'
+	aimedDepartureTime: DateTime
+	expectedDepartureTime: DateTime
+	expectedArrivalTime: DateTime
+	cancellation: boolean
+	realtime: boolean
+	quay: { __typename?: 'Quay'; publicCode: string | null; name: string }
+	destinationDisplay: {
+		__typename?: 'DestinationDisplay'
+		frontText: string | null
+		via: Array<string | null> | null
+	} | null
+	serviceJourney: {
+		__typename?: 'ServiceJourney'
+		id: string
+		transportMode: Types.TTransportMode | null
+		transportSubmode: Types.TTransportSubmode | null
+		line: {
+			__typename?: 'Line'
+			id: string
+			publicCode: string | null
+			presentation: {
+				__typename?: 'Presentation'
+				textColour: string | null
+				colour: string | null
+			} | null
+		}
+	}
+	situations: Array<{
+		__typename?: 'PtSituationElement'
+		id: string
+		description: Array<{
+			__typename?: 'MultilingualString'
+			value: string
+			language: string | null
+		}>
+		summary: Array<{ __typename?: 'MultilingualString'; value: string; language: string | null }>
+	}>
+}
 
-export type TLinesFragment = { __typename?: 'Quay', lines: Array<{ __typename?: 'Line', id: string, publicCode: string | null, name: string | null, transportMode: Types.TTransportMode | null }> };
+export type TLinesFragment = {
+	__typename?: 'Quay'
+	lines: Array<{
+		__typename?: 'Line'
+		id: string
+		publicCode: string | null
+		name: string | null
+		transportMode: Types.TTransportMode | null
+	}>
+}
 
-export type TSituationFragment = { __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> };
+export type TSituationFragment = {
+	__typename?: 'PtSituationElement'
+	id: string
+	description: Array<{ __typename?: 'MultilingualString'; value: string; language: string | null }>
+	summary: Array<{ __typename?: 'MultilingualString'; value: string; language: string | null }>
+}
 
 export type TGetQuayQueryVariables = Types.Exact<{
-  quayId: Types.Scalars['String']['input'];
-  whitelistedTransportModes?: Types.InputMaybe<Array<Types.InputMaybe<Types.TTransportMode>> | Types.InputMaybe<Types.TTransportMode>>;
-  whitelistedLines?: Types.InputMaybe<Array<Types.Scalars['ID']['input']> | Types.Scalars['ID']['input']>;
-  numberOfDepartures?: Types.InputMaybe<Types.Scalars['Int']['input']>;
-  startTime?: Types.InputMaybe<Types.Scalars['DateTime']['input']>;
-}>;
+	quayId: Types.Scalars['String']['input']
+	whitelistedTransportModes?: Types.InputMaybe<
+		Array<Types.InputMaybe<Types.TTransportMode>> | Types.InputMaybe<Types.TTransportMode>
+	>
+	whitelistedLines?: Types.InputMaybe<
+		Array<Types.Scalars['ID']['input']> | Types.Scalars['ID']['input']
+	>
+	numberOfDepartures?: Types.InputMaybe<Types.Scalars['Int']['input']>
+	startTime?: Types.InputMaybe<Types.Scalars['DateTime']['input']>
+}>
 
-
-export type TGetQuayQuery = { __typename?: 'QueryType', quay: { __typename?: 'Quay', name: string, description: string | null, publicCode: string | null, estimatedCalls: Array<{ __typename?: 'EstimatedCall', aimedDepartureTime: DateTime, expectedDepartureTime: DateTime, expectedArrivalTime: DateTime, cancellation: boolean, realtime: boolean, quay: { __typename?: 'Quay', publicCode: string | null, name: string }, destinationDisplay: { __typename?: 'DestinationDisplay', frontText: string | null, via: Array<string | null> | null } | null, serviceJourney: { __typename?: 'ServiceJourney', id: string, transportMode: Types.TTransportMode | null, transportSubmode: Types.TTransportSubmode | null, line: { __typename?: 'Line', id: string, publicCode: string | null, presentation: { __typename?: 'Presentation', textColour: string | null, colour: string | null } | null } }, situations: Array<{ __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> }> }>, situations: Array<{ __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> }>, stopPlace: { __typename?: 'StopPlace', situations: Array<{ __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> }> } | null, lines: Array<{ __typename?: 'Line', id: string, publicCode: string | null, name: string | null, transportMode: Types.TTransportMode | null }> } | null };
+export type TGetQuayQuery = {
+	__typename?: 'QueryType'
+	quay: {
+		__typename?: 'Quay'
+		name: string
+		description: string | null
+		publicCode: string | null
+		estimatedCalls: Array<{
+			__typename?: 'EstimatedCall'
+			aimedDepartureTime: DateTime
+			expectedDepartureTime: DateTime
+			expectedArrivalTime: DateTime
+			cancellation: boolean
+			realtime: boolean
+			quay: { __typename?: 'Quay'; publicCode: string | null; name: string }
+			destinationDisplay: {
+				__typename?: 'DestinationDisplay'
+				frontText: string | null
+				via: Array<string | null> | null
+			} | null
+			serviceJourney: {
+				__typename?: 'ServiceJourney'
+				id: string
+				transportMode: Types.TTransportMode | null
+				transportSubmode: Types.TTransportSubmode | null
+				line: {
+					__typename?: 'Line'
+					id: string
+					publicCode: string | null
+					presentation: {
+						__typename?: 'Presentation'
+						textColour: string | null
+						colour: string | null
+					} | null
+				}
+			}
+			situations: Array<{
+				__typename?: 'PtSituationElement'
+				id: string
+				description: Array<{
+					__typename?: 'MultilingualString'
+					value: string
+					language: string | null
+				}>
+				summary: Array<{
+					__typename?: 'MultilingualString'
+					value: string
+					language: string | null
+				}>
+			}>
+		}>
+		situations: Array<{
+			__typename?: 'PtSituationElement'
+			id: string
+			description: Array<{
+				__typename?: 'MultilingualString'
+				value: string
+				language: string | null
+			}>
+			summary: Array<{ __typename?: 'MultilingualString'; value: string; language: string | null }>
+		}>
+		stopPlace: {
+			__typename?: 'StopPlace'
+			situations: Array<{
+				__typename?: 'PtSituationElement'
+				id: string
+				description: Array<{
+					__typename?: 'MultilingualString'
+					value: string
+					language: string | null
+				}>
+				summary: Array<{
+					__typename?: 'MultilingualString'
+					value: string
+					language: string | null
+				}>
+			}>
+		} | null
+		lines: Array<{
+			__typename?: 'Line'
+			id: string
+			publicCode: string | null
+			name: string | null
+			transportMode: Types.TTransportMode | null
+		}>
+	} | null
+}
 
 export type TStopPlaceQueryVariables = Types.Exact<{
-  stopPlaceId: Types.Scalars['String']['input'];
-  whitelistedTransportModes?: Types.InputMaybe<Array<Types.InputMaybe<Types.TTransportMode>> | Types.InputMaybe<Types.TTransportMode>>;
-  whitelistedLines?: Types.InputMaybe<Array<Types.Scalars['ID']['input']> | Types.Scalars['ID']['input']>;
-  numberOfDepartures?: Types.InputMaybe<Types.Scalars['Int']['input']>;
-  startTime?: Types.InputMaybe<Types.Scalars['DateTime']['input']>;
-}>;
+	stopPlaceId: Types.Scalars['String']['input']
+	whitelistedTransportModes?: Types.InputMaybe<
+		Array<Types.InputMaybe<Types.TTransportMode>> | Types.InputMaybe<Types.TTransportMode>
+	>
+	whitelistedLines?: Types.InputMaybe<
+		Array<Types.Scalars['ID']['input']> | Types.Scalars['ID']['input']
+	>
+	numberOfDepartures?: Types.InputMaybe<Types.Scalars['Int']['input']>
+	startTime?: Types.InputMaybe<Types.Scalars['DateTime']['input']>
+}>
 
-
-export type TStopPlaceQuery = { __typename?: 'QueryType', stopPlace: { __typename?: 'StopPlace', name: string, transportMode: Array<Types.TTransportMode | null> | null, estimatedCalls: Array<{ __typename?: 'EstimatedCall', aimedDepartureTime: DateTime, expectedDepartureTime: DateTime, expectedArrivalTime: DateTime, cancellation: boolean, realtime: boolean, quay: { __typename?: 'Quay', publicCode: string | null, name: string }, destinationDisplay: { __typename?: 'DestinationDisplay', frontText: string | null, via: Array<string | null> | null } | null, serviceJourney: { __typename?: 'ServiceJourney', id: string, transportMode: Types.TTransportMode | null, transportSubmode: Types.TTransportSubmode | null, line: { __typename?: 'Line', id: string, publicCode: string | null, presentation: { __typename?: 'Presentation', textColour: string | null, colour: string | null } | null } }, situations: Array<{ __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> }> }>, situations: Array<{ __typename?: 'PtSituationElement', id: string, description: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }>, summary: Array<{ __typename?: 'MultilingualString', value: string, language: string | null }> }> } | null };
+export type TStopPlaceQuery = {
+	__typename?: 'QueryType'
+	stopPlace: {
+		__typename?: 'StopPlace'
+		name: string
+		transportMode: Array<Types.TTransportMode | null> | null
+		estimatedCalls: Array<{
+			__typename?: 'EstimatedCall'
+			aimedDepartureTime: DateTime
+			expectedDepartureTime: DateTime
+			expectedArrivalTime: DateTime
+			cancellation: boolean
+			realtime: boolean
+			quay: { __typename?: 'Quay'; publicCode: string | null; name: string }
+			destinationDisplay: {
+				__typename?: 'DestinationDisplay'
+				frontText: string | null
+				via: Array<string | null> | null
+			} | null
+			serviceJourney: {
+				__typename?: 'ServiceJourney'
+				id: string
+				transportMode: Types.TTransportMode | null
+				transportSubmode: Types.TTransportSubmode | null
+				line: {
+					__typename?: 'Line'
+					id: string
+					publicCode: string | null
+					presentation: {
+						__typename?: 'Presentation'
+						textColour: string | null
+						colour: string | null
+					} | null
+				}
+			}
+			situations: Array<{
+				__typename?: 'PtSituationElement'
+				id: string
+				description: Array<{
+					__typename?: 'MultilingualString'
+					value: string
+					language: string | null
+				}>
+				summary: Array<{
+					__typename?: 'MultilingualString'
+					value: string
+					language: string | null
+				}>
+			}>
+		}>
+		situations: Array<{
+			__typename?: 'PtSituationElement'
+			id: string
+			description: Array<{
+				__typename?: 'MultilingualString'
+				value: string
+				language: string | null
+			}>
+			summary: Array<{ __typename?: 'MultilingualString'; value: string; language: string | null }>
+		}>
+	} | null
+}
 
 export class TypedDocumentString<TResult, TVariables>
-  extends String
-  implements DocumentTypeDecoration<TResult, TVariables>
+	extends String
+	implements DocumentTypeDecoration<TResult, TVariables>
 {
-  __apiType?: DocumentTypeDecoration<TResult, TVariables>['__apiType'];
+	__apiType?: DocumentTypeDecoration<TResult, TVariables>['__apiType']
 
-  constructor(private value: string, public __meta__?: Record<string, any>) {
-    super(value);
-  }
+	constructor(
+		private value: string,
+		public __meta__?: Record<string, any>,
+	) {
+		super(value)
+	}
 
-  toString(): string & DocumentTypeDecoration<TResult, TVariables> {
-    return this.value;
-  }
+	toString(): string & DocumentTypeDecoration<TResult, TVariables> {
+		return this.value
+	}
 }
-export const SituationFragment = new TypedDocumentString(`
+export const SituationFragment = new TypedDocumentString(
+	`
     fragment situation on PtSituationElement {
   id
   description {
@@ -56,8 +266,11 @@ export const SituationFragment = new TypedDocumentString(`
     language
   }
 }
-    `, {"fragmentName":"situation"}) as unknown as TypedDocumentString<TSituationFragment, unknown>;
-export const DepartureFragment = new TypedDocumentString(`
+    `,
+	{ fragmentName: 'situation' },
+) as unknown as TypedDocumentString<TSituationFragment, unknown>
+export const DepartureFragment = new TypedDocumentString(
+	`
     fragment departure on EstimatedCall {
   quay {
     publicCode
@@ -99,8 +312,11 @@ export const DepartureFragment = new TypedDocumentString(`
     value
     language
   }
-}`, {"fragmentName":"departure"}) as unknown as TypedDocumentString<TDepartureFragment, unknown>;
-export const LinesFragment = new TypedDocumentString(`
+}`,
+	{ fragmentName: 'departure' },
+) as unknown as TypedDocumentString<TDepartureFragment, unknown>
+export const LinesFragment = new TypedDocumentString(
+	`
     fragment lines on Quay {
   lines {
     id
@@ -109,7 +325,9 @@ export const LinesFragment = new TypedDocumentString(`
     transportMode
   }
 }
-    `, {"fragmentName":"lines"}) as unknown as TypedDocumentString<TLinesFragment, unknown>;
+    `,
+	{ fragmentName: 'lines' },
+) as unknown as TypedDocumentString<TLinesFragment, unknown>
 export const GetQuayQuery = new TypedDocumentString(`
     query getQuay($quayId: String!, $whitelistedTransportModes: [TransportMode], $whitelistedLines: [ID!], $numberOfDepartures: Int = 20, $startTime: DateTime) {
   quay(id: $quayId) {
@@ -185,7 +403,7 @@ fragment situation on PtSituationElement {
     value
     language
   }
-}`) as unknown as TypedDocumentString<TGetQuayQuery, TGetQuayQueryVariables>;
+}`) as unknown as TypedDocumentString<TGetQuayQuery, TGetQuayQueryVariables>
 export const StopPlaceQuery = new TypedDocumentString(`
     query StopPlace($stopPlaceId: String!, $whitelistedTransportModes: [TransportMode], $whitelistedLines: [ID!], $numberOfDepartures: Int = 20, $startTime: DateTime) {
   stopPlace(id: $stopPlaceId) {
@@ -246,4 +464,4 @@ fragment situation on PtSituationElement {
     value
     language
   }
-}`) as unknown as TypedDocumentString<TStopPlaceQuery, TStopPlaceQueryVariables>;
+}`) as unknown as TypedDocumentString<TStopPlaceQuery, TStopPlaceQueryVariables>
