d569bdf3eb8ab405d00babc8390c23f9e36b9a78
Maiken Lie <maiken.lie@bekk.no>
2026-08-06 08:53:19 +0200

    remove norwegian summary and norweagian description

--- src/Board/scenarios/Table/components/TileSituations.tsx
+++ src/Board/scenarios/Table/components/TileSituations.tsx
@@ -15,20 +15,16 @@ function getSituationText(
 	const preferredSummary = situation?.summary.find(
 		(summary) => summary.language === preferredLanguage,
 	)?.value
-	const norwegianSummary =
-		preferredLanguage === 'no'
-			? preferredSummary
-			: situation?.summary.find((summary) => summary.language === 'no')?.value
 
+	const norwegianSummary = situation?.summary.find((summary) => summary.language === 'no')?.value
 	const summary = preferredSummary ?? norwegianSummary ?? situation?.summary[0]?.value
 
 	const preferredDescription = situation?.description.find(
 		(description) => description.language === preferredLanguage,
 	)?.value
-	const norwegianDescription =
-		preferredLanguage === 'no'
-			? preferredDescription
-			: situation?.description.find((description) => description.language === 'no')?.value
+	const norwegianDescription = situation?.description.find(
+		(description) => description.language === 'no',
+	)?.value
 	const description =
 		preferredDescription ?? norwegianDescription ?? situation?.description[0]?.value
 
