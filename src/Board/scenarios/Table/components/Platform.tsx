f73f6bb3605936a33862cfae43b256d0c77c636f
Magnus Ouren <91123056+magnusouren@users.noreply.github.com>
2026-09-01 11:13:35 +0200

    Oppdater plattformkomponenten for å bruke getPlatformLabel og håndtere railReplacementBus som rail

--- src/Board/scenarios/Table/components/Platform.tsx
+++ src/Board/scenarios/Table/components/Platform.tsx
@@ -1,10 +1,13 @@
 import { useNonNullContext } from '@/Shared/hooks/useNonNullContext'
 import { nanoid } from 'nanoid'
 import { useBoardContext } from '@/Board/context'
-import { getColumnLabel } from '@/Shared/utils/translations'
+
 import { DeparturesContext } from '../contexts'
 import { TableCell } from './TableCell'
 import { TableColumn } from './TableColumn'
+
+import { getPlatformLabel } from '@/Shared/utils/translations'
+import type { TTransportMode } from '@/types/graphql-schema'
 
 function Platform() {
 	const departures = useNonNullContext(DeparturesContext)
@@ -13,11 +16,26 @@ function Platform() {
 	const platforms = departures.map((departure) => ({
 		publicCode: departure.quay.publicCode,
 		key: nanoid(),
+		transportMode: departure.serviceJourney.transportMode,
+		subMode: departure.serviceJourney.transportSubmode,
 	}))
 
+	// Overskriver transportMode for railReplacementBus to rail for platform fordi vi ønsker å vise "Spor"
+	const updatedPlatforms = platforms.map((platform) => {
+		if (platform.transportMode === 'bus' && platform.subMode === 'railReplacementBus') {
+			return { ...platform, transportMode: 'rail' as TTransportMode }
+		}
+		return platform
+	})
+
+	const title = getPlatformLabel(
+		updatedPlatforms.map((platform) => platform.transportMode || 'unknown'),
+		language,
+	)
+
 	return (
-		<TableColumn title={getColumnLabel('platform', language)}>
-			{platforms.map((platform) => (
+		<TableColumn title={title}>
+			{updatedPlatforms.map((platform) => (
 				<TableCell key={platform.key}>{platform.publicCode}</TableCell>
 			))}
 		</TableColumn>
