4bfb559ed89056f23c2b530193494744ff90336c
Agnes H. Birkemo <125461284+agnesbirkemo@users.noreply.github.com>
2026-09-23 10:11:48 +0200

    📈 Logge fra tavla-visning når graphQL-kall feiler (#155)
    ### 🥅 Bakgrunn
    
    > Som en tavlis
    Ønsker jeg at feil som oppstår i forbindelse med fetch fra
    Journey-planner blir logget
    Slik at jeg har en oversikt over hvor ofte dette skjer
    
    ### ✨ Løsning
    
    - la til et kall til reportError I BoardTile som sender med boardId
    (lagt til i Context), en ny errorCode (fetch_journey_planner) og
    error-melding.
    
    
    ### ✅ Sjekkliste
    
    - [ ] Testet i Chrome, Firefox og Safari
    - [ ] Testet i BrowserStack
    - [ ] Oppdatert dokumentasjon (hvis relevant)

--- src/Board/context.ts
+++ src/Board/context.ts
@@ -2,11 +2,13 @@ import type { BoardLanguage } from '@/Shared/types/db-types/boards'
 import { createContext, useContext } from 'react'
 
 type BoardContextValue = {
+	boardId: string | undefined
 	isArrivals: boolean
 	language: BoardLanguage
 }
 
 const BoardContext = createContext<BoardContextValue>({
+	boardId: undefined,
 	isArrivals: false,
 	language: 'nb',
 })
